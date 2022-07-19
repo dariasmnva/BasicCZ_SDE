5cb96810556c92b220e04e880e877e03bfd645d5
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:55:05 +0700

    027

---
+++ 027/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+
+Console.WriteLine ("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите степень B");
+int B = Convert.ToInt32(Console.ReadLine());
+int c=A;
+for(int i=1;i<B;i++)
+{
+    A=A*c;
+}
+Console.WriteLine(A);
