9653e08f6dcff942b69cea909e2e266308032b05
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:51:17 +0700

    013

---
+++ 013/Program.cs
@@ -0,0 +1,13 @@
+﻿//Удалить вторую цифру целого числа введенного с клавиатуры.
+
+int x = Convert.ToInt32(Console.ReadLine()); 
+int s = Convert.ToInt32(Console.ReadLine());
+int m =1;
+while(s>1)
+{
+    m *=10;
+    s --;
+}
+
+int t=(x%m)+(x/(m*10))*m;
+Console.WriteLine(t);
