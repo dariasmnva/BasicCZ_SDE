b6c48896ef062ed06a9379ec370079ffe42bc4e2
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:54:24 +0700

    024

---
+++ 024/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите кол-во чисел для вывода:");
+int N= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число"+"/"+"Квадрат");
+for(int i=1; i<=N;i++)
+Console.WriteLine(i+"/"+i*i);
+
