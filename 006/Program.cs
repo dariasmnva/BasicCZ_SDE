6a8362cfa50a4212c518855385961fc4613ea583
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:49:24 +0700

    006

---
+++ 006/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+
+double a;
+double y;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+y=Math.Sin(a);
+Console.WriteLine ($"y={y}");
