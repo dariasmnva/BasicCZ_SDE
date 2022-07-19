785e8fd4204ba20ae25618e2a7d993c2e3d5d167
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:45:50 +0700

    002

---
+++ 002/Program.cs
@@ -0,0 +1,24 @@
+﻿/* По двум заданным числам проверить является ли 
+первое квадратом второго */
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a*a==b)
+{
+    Console.WriteLine("b is square a");
+}
+else
+{
+    Console.WriteLine("b is't square a");
+}
+if (a==b*b)
+{
+    Console.WriteLine("a is square b");
+}
+else
+{
+    Console.WriteLine("a is't square b");
+}
