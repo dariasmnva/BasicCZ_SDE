4d74772a2ae42b59ce264dec7374ac99360c3b4f
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:51:53 +0700

    015

---
+++ 015/Program.cs
@@ -0,0 +1,15 @@
+﻿/*С клавиатуры вводится целое число. 
+Вывести третью цифру числа или сообщить, что её нет.
+*/
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if(a>99)
+{
+    Console.WriteLine(a%10);
+}
+else
+{
+    Console.WriteLine($"Третьей цифры числа нет");
+}
