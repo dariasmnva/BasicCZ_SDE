3bf40e9a0980ce4e4c80cfb5e26f0e436477aea6
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:49:49 +0700

    008

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вывести на экран числа от -N до N
+
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+for(int i=a;i<=b; i++)
+{
+    Console.Write($"{i} ");
+}
+Console.ReadKey(true);
