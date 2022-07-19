72836336fa336309f39f122378e46aa63488faf3
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:49:01 +0700

    005

---
+++ 005/Program.cs
@@ -0,0 +1,21 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+double a, b, c;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+s=Console.ReadLine();
+c=Convert.ToDouble(s);
+if(a>b&&a>c) 
+{
+    Console.WriteLine($"max={a}");
+}
+else if (b>a&&b>c)
+{
+    Console.WriteLine($"max={b}");
+}
+else
+{
+    Console.WriteLine($"max={c}");
+}
