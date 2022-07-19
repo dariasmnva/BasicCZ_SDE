1a1aa900c171707c1e122e6e5e833a879bcd1c6f
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:54:55 +0700

    026

---
+++ 026/Program.cs
@@ -0,0 +1,6 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите кол-во чисел для вывода:");
+int A= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {A}:");
+Console.Write(A*(A+1)/2);
