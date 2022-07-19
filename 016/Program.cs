5b60ee55c0c707ef1d0a32276d5a93882e05947b
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:52:06 +0700

    016

---
+++ 016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int a =Convert.ToInt32(Console.ReadLine());
+bool b=a%7==0 && a%23==0;
+if (b)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
