ae629c31e1e34687ff256b9d4ecc3c0592777ffb
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:55:17 +0700

    028

---
+++ 028/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+
+Console.WriteLine(CountDigits(0));
+
+
+
+
+int CountDigits(int N)
+{
+    if(N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N/=10;
+    }
+    return k;
+}
+
