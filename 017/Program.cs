cb1a7990dcb3c63eb7ef6291981c7f4e2dd74372
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:52:15 +0700

    017

---
+++ 017/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. 
+// Выяснить является номер дня недели выходным.
+
+int a =Convert.ToInt32(Console.ReadLine());
+bool b=a==6, c=a==7;
+if (b||c)
+{
+    System.Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+}
