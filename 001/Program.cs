6248a0deb97cf602910c9257e2fd62b3dffd09aa
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:44:16 +0700

    001

---
+++ 001/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+int a = 2, b =3 ;
+int s = a + b;
+Console.WriteLine($"{a}+{b}={s}");
+*/
+
+// Ввести с клавиатуры целое число и вывести квадрат этого числа на экран 
+int a;
+int b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+Console.WriteLine($"{a}^2={b}");
+
