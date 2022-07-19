08b8da9e41af3292f93fa4428f70973abbe3b5b3
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:53:48 +0700

    021

---
+++ 021/Program.cs
@@ -0,0 +1,26 @@
+﻿// See Задать номер четверти, 
+// показать диапазоны для возможных координат.
+
+Console.WriteLine($"Введите номер четверти от 1 до 4");
+int a = Convert.ToInt32(Console.ReadLine());
+bool b=a==1, c=a==2, d=a==3, e=a==4;
+if (b)
+{
+    Console.WriteLine($"Диапозон x>0,y>0");
+}
+else if (c)
+{
+    Console.WriteLine($"Диапозон x<0, y>0");
+}
+else if (d)
+{
+    Console.WriteLine($"Диапозон x<0, y<0");
+}
+else if (e)
+{
+    Console.WriteLine($"Диапозон x>0, y<0");
+}
+else
+{
+    Console.WriteLine($"Ошибка!");
+}
