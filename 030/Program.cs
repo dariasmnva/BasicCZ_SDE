78d719e61f7a70e6ddf7f6993f745419b01021f5
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:55:38 +0700

    030

---
+++ 030/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите кол-во чисел для вывода:");
+int N= Convert.ToInt32(Console.ReadLine());
+int p=1;
+Console.WriteLine($"Произведение чисел от 1 до {N}:");
+for (int i=1;i<=N;i++)
+{
+    p=p*i;
+}
+Console.WriteLine(p);
