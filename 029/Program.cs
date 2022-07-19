d307497ee50610b7b6c3b8e64fb9f7b847b89ad9
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:55:27 +0700

    029

---
+++ 029/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a>0)
+{
+    sum += a%10;
+    a /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
