a424f1b85f9ebe7bc4200e362ac33b6008b92606
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:55:51 +0700

    031

---
+++ 031/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+Console.WriteLine("Введите кол-во чисел для вывода:");
+int N= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число"+"/"+"Куб");
+for(int i=1; i<=N;i++)
+{
+ if(i%2 == 0)
+ {
+    
+    Console.WriteLine(i + "/" + i*i*i);
+ }
+}
