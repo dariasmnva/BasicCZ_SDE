566e39fdef54e7a7d730f6ca2be6d8f09af2b37c
dariasmnva <dariaivnv@yandex.ru>
2022-07-19 11:49:36 +0700

    007

---
+++ 007/Program.cs
@@ -0,0 +1,14 @@
+﻿//Выяснить является ли число чётным.
+
+int a;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+if(a%2 == 0)
+{
+    Console.WriteLine($"{a}=четное число");
+}
+else 
+{
+    Console.WriteLine($"{a}=нечетное число");
+}
