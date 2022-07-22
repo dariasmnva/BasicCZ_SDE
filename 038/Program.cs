03271de399ab8819a2bcdfa2a0f1b3468532ba77
dariasmnva <dariaivnv@yandex.ru>
2022-07-22 16:00:54 +0700

    038

---
+++ 038/Program.cs
@@ -0,0 +1,23 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+
+int N=123;
+int [] a= new int[N];
+Random random = new Random();
+for(int i=0;i<a.Length; i++)
+    a[i]=random.Next(10,999);
+
+int k=0;
+
+for(int i=0; i<a.Length;i++)   
+    if(a[i]>11 && a[i]<100)
+    {
+        k++;
+    }
+
+
+ for(int i=0; i<a.Length;i++)   
+     Console.Write($"{a[i],4}");   
+Console.WriteLine();  
+Console.WriteLine($"Количество элементов из отрезка [10,99]={k}");   
+ 
