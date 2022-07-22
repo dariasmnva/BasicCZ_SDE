416b2dc7c52efec029dc52c046195f1ab302f62e
dariasmnva <dariaivnv@yandex.ru>
2022-07-22 16:00:25 +0700

    035

---
+++ 035/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+
+
+int N=8;
+int [] a= new int[N];
+Random random = new Random();
+for(int i=0;i<a.Length; i++)
+    a[i]=random.Next(0,11);
+
+
+int [] b=new int[N];
+for(int i=0; i<a.Length;i++)
+    if (a[i]>0) b[i]=-a[i];
+
+
+
+ for(int i=0; i<a.Length;i++)   
+     Console.Write($"{a[i],4}");   
+Console.WriteLine();     
+ for(int i=0; i<b.Length;i++)   
+     Console.Write($"{b[i],4}"); 
+
