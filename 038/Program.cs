58b0415a5d4127a4fa7ac35355942954327b0de7
dariasmnva <dariaivnv@yandex.ru>
2022-08-14 23:22:36 +0700

    038

--- 038/Program.cs
+++ 038/Program.cs
@@ -1,23 +1,36 @@
 ﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
+ 
+ 
+int [] a;
+Init(out a);
+int k;
+Solve(a,out k);
+Print(a);
+Console.WriteLine();  
+Console.WriteLine($"Количество элементов из отрезка [10,99]={k}"); 
 
-int N=123;
-int [] a= new int[N];
-Random random = new Random();
-for(int i=0;i<a.Length; i++)
-    a[i]=random.Next(10,999);
+void Init(out int [] a,int Lenght=50, int min=10,int max=999)
+{
+    a=new int[Lenght];
+    Random random = new Random();
+    for(int i=0;i<a.Length; i++)
+    a[i]=random.Next(min,max+1);
+}    
 
-int k=0;
 
-for(int i=0; i<a.Length;i++)   
+void Solve(int [] a, out int k)
+{
+    k=0;
+    for(int i=0; i<a.Length;i++)
     if(a[i]>11 && a[i]<100)
     {
         k++;
     }
+}
 
-
- for(int i=0; i<a.Length;i++)   
-     Console.Write($"{a[i],4}");   
-Console.WriteLine();  
-Console.WriteLine($"Количество элементов из отрезка [10,99]={k}");   
- 
+void Print(int[] a)
+{
+    for (int i=0; i<a.Length; i++)
+    Console.Write($"{a[i],4}");
+}
