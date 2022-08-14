c7ee39bbb66228b7c089f30931156e64c067a912
dariasmnva <dariaivnv@yandex.ru>
2022-08-14 23:21:52 +0700

    035

--- 035/Program.cs
+++ 035/Program.cs
@@ -1,23 +1,33 @@
 ﻿// Написать программу замены элементов массива на противоположные
 
 
-
-int N=8;
-int [] a= new int[N];
-Random random = new Random();
-for(int i=0;i<a.Length; i++)
-    a[i]=random.Next(0,11);
-
-
-int [] b=new int[N];
-for(int i=0; i<a.Length;i++)
-    if (a[i]>0) b[i]=-a[i];
+int [] a;
+Init(out a);
+PrintPos(a);
+Console.WriteLine();
+PrintNeg(a);
 
 
+void Init(out int [] a,int Lenght=8, int min=0,int max=10)
+{
+    a=new int[Lenght];
+    Random random = new Random();
+    for(int i=0;i<a.Length; i++)
+    a[i]=random.Next(min,max+1);
+}    
 
- for(int i=0; i<a.Length;i++)   
-     Console.Write($"{a[i],4}");   
-Console.WriteLine();     
- for(int i=0; i<b.Length;i++)   
-     Console.Write($"{b[i],4}"); 
+
+
+void PrintPos(int[] a)
+{
+    for (int i=0; i<a.Length; i++)
+    Console.Write($"{a[i],4}");
+    
+}
+void PrintNeg(int[] a)
+{
+    for (int i=0; i<a.Length; i++)
+    Console.Write($"{-a[i],4}");
+    
+}
 
