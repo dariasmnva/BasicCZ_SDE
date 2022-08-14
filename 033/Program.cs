780b4ebb29d48a979c8fd84c7cbf38c8f0e802f6
dariasmnva <dariaivnv@yandex.ru>
2022-08-14 23:21:18 +0700

    033

--- 033/Program.cs
+++ 033/Program.cs
@@ -1,12 +1,20 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
 
-int N=8;
-int [] a= new int[N];
-Random random = new Random();
-for(int i=0;i<a.Length; i++)
-    a[i]=random.Next(0,2);
+int [] a;
+Init(out a);
+Print(a);
 
-for (int i=0; i<a.Length; i++)
+void Init(out int [] a,int Lenght=8, int min=0,int max=1)
+{
+    a=new int[Lenght];
+    Random random = new Random();
+    for(int i=0;i<a.Length; i++)
+    a[i]=random.Next(min,max+1);
+}    
+void Print(int[] a)
+{
+    for (int i=0; i<a.Length; i++)
     Console.Write($"{a[i],4}");
+}
 
