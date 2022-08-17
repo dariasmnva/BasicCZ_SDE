8d21d3ecfb4ad6b98c5bb25b55bee48b6fe66fef
dariasmnva <dariaivnv@yandex.ru>
2022-08-17 18:30:11 +0700

    055

---
+++ 055/Program.cs
@@ -0,0 +1,31 @@
+﻿// Дан целочисленный массив. 
+//Найти среднее арифметическое каждого из столбцов.
+
+
+int n = Int32.Parse(Console.ReadLine());
+            
+            int[,] arr = new int[n, n];
+            Random random = new Random();
+ 
+            for (short i = 0; i < n; i++)
+                for (short j = 0; j < n; j++)
+                    arr[i, j] = random.Next(1, 20);
+ 
+            for(short i = 0; i < n; i++)
+            {
+                for (short j = 0; j < n; j++)
+                    Console.Write(arr[i,j] + " ");
+                Console.WriteLine();
+            }
+ 
+            for (short i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (short j = 0; j < n; j++)
+                {
+                    sum += arr[j, i];
+                }
+                Console.WriteLine("Среднее арифметическое в {0} столбце: {1}", i, sum/n);
+            }
+
+
