a0238d9c2c092eb9fe209289093d9a447e520433
dariasmnva <dariaivnv@yandex.ru>
2022-08-23 00:22:40 +0700

    Recursion

---
+++ DZ7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления функции Аккермана
+
+ int Depth = 0;
+ UInt64 AckermannFunc(UInt64 m, UInt64 n) 
+ {
+            Depth++;
+            if (m == 0)
+                return n + 1;
+            if (n == 0)
+                return AckermannFunc(m - 1, 1);
+            return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+}
+ 
+void Main(string[] args) 
+{
+            Console.WriteLine(AckermannFunc(3, 5));
+            Console.WriteLine(Depth);
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,52 @@
+﻿// Написать программу вывода чисел от  1 до N 
+//без использования циклов и оператора goto.
+
+void Loop(int i,int N)
+{
+    System.Console.WriteLine(i);
+    if (i<N) Loop(i+1,N);//рекурсия
+}
+
+ int F(int n)
+ {
+    if (n>0) return F(n-1)*n;
+    else 
+        return 1;
+ }
+
+ int Sum(int i,int N)
+ {
+    if (i<=N) return Sum(i+1,N)+i;
+    else
+     return 0;
+ }
+ 
+//System.Console.WriteLine(Sum(0,4));
+
+
+/*
+System.Console.WriteLine(F(5));
+int N=5;
+int f=1;
+for(int i=1;i<=N;i++)
+    f=f*i;
+System.Console.WriteLine(f);
+*/
+//Loop(1,10);
+
+
+
+
+
+
+
+
+
+/* !!!!!!!!!DANGER!!!!!!!!!!!
+int i=0;
+label:
+    i++;
+    System.Console.Write(1);
+if (i<10) goto label;//оператор безусловного перехода.
+//Ctrl+C
+*/
