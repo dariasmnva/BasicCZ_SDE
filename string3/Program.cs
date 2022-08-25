d0e4854ad5db2cfc85e0031d36ac8432c6b3988e
dariasmnva <dariaivnv@yandex.ru>
2022-08-26 01:18:10 +0700

    string3

---
+++ string3/Program.cs
@@ -0,0 +1,8 @@
+﻿// С клавиатуры вводится строка разделенная точкой. 
+//Подсчитать количество символов до точки.
+
+
+using System;
+string str=Console.ReadLine();
+int countBefore = str.IndexOf('.');
+Console.WriteLine($"Количество символов до точки = {countBefore}");
