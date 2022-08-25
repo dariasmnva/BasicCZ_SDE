b66d7bd74fc53e0d88a1424e1d1d072d53ae9448
dariasmnva <dariaivnv@yandex.ru>
2022-08-26 00:27:27 +0700

    066

---
+++ 066/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+int Sum(int i,int N)
+ {
+    if (i<=N) return Sum(i+1,N)+i;
+    else
+     return 0;
+ }
+ 
+Console.WriteLine(Sum(M,N));
+
