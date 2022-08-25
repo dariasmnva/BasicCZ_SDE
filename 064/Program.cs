04dfaa08a28d0f9e49d584de30e5878595ba852e
dariasmnva <dariaivnv@yandex.ru>
2022-08-26 00:27:16 +0700

    064

---
+++ 064/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+Loop(M,N);
+void Loop(int i,int N)
+{
+    System.Console.Write($" {i}");
+    if (i<N) Loop(i+1,N);
+}
